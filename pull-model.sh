706156c0d7a4020a4e5cd7b29231fe1f90a7a999
Sagar Adhikari <sagar.adhikari@steep.de>
2025-02-17 11:13:30 +0100

    Update for Deepseek

--- pull-model.sh
+++ pull-model.sh
@@ -13,6 +13,27 @@ pid=$!
 
 sleep 5
 
-echo "Pulling llama3 model"
+echo ".......................Pulling llama3.2:3b model..............."
+ollama pull llama3.2:3b
+
+echo ".......................Pulling llama3.1:8b model..............."
 ollama pull llama3.1:8b
+
+#echo ".......................Pulling llama3.3 model..............."
+#ollama pull llama3.3
+
+echo ".......................pulling deepseek-r1:32b model..............."
+ollama pull deepseek-r1:32b
+
+echo ".......................Pulling embedding model..........."
+ollama pull nomic-embed-text
+
+echo ".......................bge-m3..........."
+ollama pull bge-m3
+
+echo ".......................bge-large..........."
+ollama pull bge-large
+
+echo ".......................jina-embeddings-v2-base-de..........."
+ollama pull jina/jina-embeddings-v2-base-de
 wait $pid

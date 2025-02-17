9dda8478096c86aa5c39cb83dcf12a2fcb9b1afc
Sagar Adhikari <sagar.adhikari@steep.de>
2025-02-17 11:14:16 +0100

    Update create and drop Tables from postgresql database

--- app/scripts/create_tables.py
+++ app/scripts/create_tables.py
@@ -9,12 +9,34 @@ backend_dir = os.path.abspath(os.path.join(current_dir, os.pardir, os.pardir))
 sys.path.append(backend_dir)
 
 from app.database.config import engine, Base
-from app.models.models import User, Chat
+from sqlalchemy import inspect
+from sqlalchemy.exc import SQLAlchemyError
+from app.models.models import User, ChatHistory, pgDocument  # should always be imported if u want to create tables
 
+# Test if we can connect to the database
+try:
+    # Test if the connection to the engine works
+    with engine.connect() as connection:
+        print("Successfully connected to the database.")
+except SQLAlchemyError as e:
+    print(f"Error connecting to the database: {e}")
 
+# Create the tables
 def create_tables():
-    Base.metadata.create_all(bind=engine)
-    print("Tables created successfully.")
+    try:
+        Base.metadata.create_all(bind=engine)  # Create tables based on the models
+        print("Tables created successfully.")
+    except SQLAlchemyError as e:
+        print(f"Error creating tables: {e}")
+
+# Check the existing tables
+def check_existing_tables():
+    inspector = inspect(engine)
+    tables = inspector.get_table_names()
+    print(f"Existing tables in the database: {tables}")
+    return tables
 
 if __name__ == "__main__":
     create_tables()
+    tables = check_existing_tables()
+    print(f"Tables in the database after creation: {tables}")
--- app/scripts/drop_tables.py
+++ app/scripts/drop_tables.py
@@ -1,5 +1,17 @@
-from Backend.app.database.config import engine, Base
+
+import sys
+import os
+
+# Get the path to the backend directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+backend_dir = os.path.abspath(os.path.join(current_dir, os.pardir, os.pardir))
+
+# Add the backend directory to the Python path
+sys.path.append(backend_dir)
+
+from app.database.config import engine, Base
 from sqlalchemy import Table, MetaData
+from app.models.models import User, ChatHistory  # Import your models here
 
 metadata = MetaData()
 
@@ -14,7 +26,7 @@ def drop_tables():
 def drop_specific_tables():
     # Specify the tables you want to drop
     user_table = Table('users', metadata, autoload_with=engine)
-    chat_table = Table('chats', metadata, autoload_with=engine)
+    chat_table = Table('chat_history', metadata, autoload_with=engine)
 
     # Drop the tables
     user_table.drop(bind=engine)
